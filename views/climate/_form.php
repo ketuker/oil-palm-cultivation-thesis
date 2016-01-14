dcc043d6add21b230e4c7fb7206a03b2858b2d06
Habib Royni Mujtahid <habib@bukapeta.com>
2016-01-15 00:24:55 +0700

    Merubah metode form climate, sekarang data yang dikirim sudah dalam bentuk value asli

--- views/climate/_form.php
+++ views/climate/_form.php
@@ -23,66 +23,14 @@ use kartik\slider\Slider;
         'name'=>'CH_TEMP',
         'value'=>10,
         'pluginEvents' => [
-            'slideStop' => "function(val) { document.getElementById('climate-ch_temp').value = val.value; }",
+            'slideStop' => "function(val) { document.getElementById('climate-ch_temp').value = ConvertNumber(val.value); }",
         ],
         'pluginOptions'=>[
             'min'=>2,
             'max'=>18,
             'step'=>1,
             'tooltip'=>'always',
-            'formatter'=>new yii\web\JsExpression("function(val) { 
-                if (val == 2) {
-                    return '1/9';
-                }
-                if (val == 3) {
-                    return '1/8';
-                }
-                if (val == 4) {
-                    return '1/7';
-                }
-                if (val == 5) {
-                    return '1/6';
-                }
-                if (val == 6) {
-                    return '1/5';
-                }
-                if (val == 7) {
-                    return '1/4';
-                }
-                if (val == 8) {
-                    return '1/3';
-                }
-                if (val == 9) {
-                    return '1/2';
-                }
-                if (val == 10) {
-                    return '1';
-                }
-                if (val == 11) {
-                    return '2/1';
-                }
-                if (val == 12) {
-                    return '3/1';
-                }
-                if (val == 13) {
-                    return '4/1';
-                }
-                if (val == 14) {
-                    return '5/1';
-                }
-                if (val == 15) {
-                    return '6/1';
-                }
-                if (val == 16) {
-                    return '7/1';
-                }
-                if (val == 17) {
-                    return '8/1';
-                }
-                if (val == 18) {
-                    return '9/1';
-                }
-            }")
+            'formatter'=>new yii\web\JsExpression("function(val) { return ConvertString(val); }")
         ]
     ]) . '<b class="badge"> Temp</b> '; ?>
 
@@ -90,66 +38,14 @@ use kartik\slider\Slider;
         'name'=>'CH_DM',
         'value'=>10,
         'pluginEvents' => [
-            'slideStop' => "function(val) { document.getElementById('climate-ch_dm').value = val.value; }",
+            'slideStop' => "function(val) { document.getElementById('climate-ch_dm').value = ConvertNumber(val.value); }",
         ],
         'pluginOptions'=>[
             'min'=>2,
             'max'=>18,
             'step'=>1,
             'tooltip'=>'always',
-            'formatter'=>new yii\web\JsExpression("function(val) { 
-                if (val == 2) {
-                    return '1/9';
-                }
-                if (val == 3) {
-                    return '1/8';
-                }
-                if (val == 4) {
-                    return '1/7';
-                }
-                if (val == 5) {
-                    return '1/6';
-                }
-                if (val == 6) {
-                    return '1/5';
-                }
-                if (val == 7) {
-                    return '1/4';
-                }
-                if (val == 8) {
-                    return '1/3';
-                }
-                if (val == 9) {
-                    return '1/2';
-                }
-                if (val == 10) {
-                    return '1';
-                }
-                if (val == 11) {
-                    return '2/1';
-                }
-                if (val == 12) {
-                    return '3/1';
-                }
-                if (val == 13) {
-                    return '4/1';
-                }
-                if (val == 14) {
-                    return '5/1';
-                }
-                if (val == 15) {
-                    return '6/1';
-                }
-                if (val == 16) {
-                    return '7/1';
-                }
-                if (val == 17) {
-                    return '8/1';
-                }
-                if (val == 18) {
-                    return '9/1';
-                }
-            }")
+            'formatter'=>new yii\web\JsExpression("function(val) { return ConvertString(val); }")
         ]
     ]) . '<b class="badge"> DM</b> '; ?>
 
@@ -157,66 +53,14 @@ use kartik\slider\Slider;
         'name'=>'TEMP_DM',
         'value'=>10,
         'pluginEvents' => [
-            'slideStop' => "function(val) { document.getElementById('climate-temp_dm').value = val.value; }",
+            'slideStop' => "function(val) { document.getElementById('climate-temp_dm').value = ConvertNumber(val.value); }",
         ],
         'pluginOptions'=>[
             'min'=>2,
             'max'=>18,
             'step'=>1,
             'tooltip'=>'always',
-            'formatter'=>new yii\web\JsExpression("function(val) { 
-                if (val == 2) {
-                    return '1/9';
-                }
-                if (val == 3) {
-                    return '1/8';
-                }
-                if (val == 4) {
-                    return '1/7';
-                }
-                if (val == 5) {
-                    return '1/6';
-                }
-                if (val == 6) {
-                    return '1/5';
-                }
-                if (val == 7) {
-                    return '1/4';
-                }
-                if (val == 8) {
-                    return '1/3';
-                }
-                if (val == 9) {
-                    return '1/2';
-                }
-                if (val == 10) {
-                    return '1';
-                }
-                if (val == 11) {
-                    return '2/1';
-                }
-                if (val == 12) {
-                    return '3/1';
-                }
-                if (val == 13) {
-                    return '4/1';
-                }
-                if (val == 14) {
-                    return '5/1';
-                }
-                if (val == 15) {
-                    return '6/1';
-                }
-                if (val == 16) {
-                    return '7/1';
-                }
-                if (val == 17) {
-                    return '8/1';
-                }
-                if (val == 18) {
-                    return '9/1';
-                }
-            }")
+            'formatter'=>new yii\web\JsExpression("function(val) { return ConvertString(val); }")
         ]
     ]) . '<b class="badge"> DM</b> '; ?>
 
@@ -227,3 +71,113 @@ use kartik\slider\Slider;
     <?php ActiveForm::end(); ?>
 
 </div>
+
+<script type="text/javascript">
+    function ConvertString(paramkiriman){
+        if (paramkiriman == 2) {
+            return '1/9';
+        }
+        if (paramkiriman == 3) {
+            return '1/8';
+        }
+        if (paramkiriman == 4) {
+            return '1/7';
+        }
+        if (paramkiriman == 5) {
+            return '1/6';
+        }
+        if (paramkiriman == 6) {
+            return '1/5';
+        }
+        if (paramkiriman == 7) {
+            return '1/4';
+        }
+        if (paramkiriman == 8) {
+            return '1/3';
+        }
+        if (paramkiriman == 9) {
+            return '1/2';
+        }
+        if (paramkiriman == 10) {
+            return '1';
+        }
+        if (paramkiriman == 11) {
+            return '2/1';
+        }
+        if (paramkiriman == 12) {
+            return '3/1';
+        }
+        if (paramkiriman == 13) {
+            return '4/1';
+        }
+        if (paramkiriman == 14) {
+            return '5/1';
+        }
+        if (paramkiriman == 15) {
+            return '6/1';
+        }
+        if (paramkiriman == 16) {
+            return '7/1';
+        }
+        if (paramkiriman == 17) {
+            return '8/1';
+        }
+        if (paramkiriman == 18) {
+            return '9/1';
+        }
+    }
+
+    function ConvertNumber(paramkiriman){
+        if (paramkiriman == 2) {
+            return 1/9;
+        }
+        if (paramkiriman == 3) {
+            return 1/8;
+        }
+        if (paramkiriman == 4) {
+            return 1/7;
+        }
+        if (paramkiriman == 5) {
+            return 1/6;
+        }
+        if (paramkiriman == 6) {
+            return 1/5;
+        }
+        if (paramkiriman == 7) {
+            return 1/4;
+        }
+        if (paramkiriman == 8) {
+            return 1/3;
+        }
+        if (paramkiriman == 9) {
+            return 1/2;
+        }
+        if (paramkiriman == 10) {
+            return 1;
+        }
+        if (paramkiriman == 11) {
+            return 2/1;
+        }
+        if (paramkiriman == 12) {
+            return 3/1;
+        }
+        if (paramkiriman == 13) {
+            return 4/1;
+        }
+        if (paramkiriman == 14) {
+            return 5/1;
+        }
+        if (paramkiriman == 15) {
+            return 6/1;
+        }
+        if (paramkiriman == 16) {
+            return 7/1;
+        }
+        if (paramkiriman == 17) {
+            return 8/1;
+        }
+        if (paramkiriman == 18) {
+            return 9/1;
+        }
+    }
+</script>
