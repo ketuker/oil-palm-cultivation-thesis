83a7376ec1f574f84c80b29c26eb199b131deb22
Habib Royni Mujtahid <habib@bukapeta.com>
2016-01-17 14:34:22 +0700

    finish until bobot for climate factor

--- controllers/ClimateController.php
+++ controllers/ClimateController.php
@@ -68,18 +68,50 @@ class ClimateController extends Controller
             $temp_ch    = 1 / $ch_temp;
             $ch_ch      = 1;
 
-            $ch_dm      = $_POST['Climate']['ch_dm'];
-            $dm_ch      = 1 / $ch_dm;
-            $dm_dm      = 1;
-
             $temp_dm    = $_POST['Climate']['temp_dm'];
             $dm_temp    = 1 / $temp_dm;
             $temp_temp  = 1;
 
-            $sum_column_ch      = $ch_ch + $ch_temp + $temp_ch;
-            $sum_column_dm      = $dm_dm + $temp_dm + $dm_ch;
-            $sum_column_temp    = $temp_temp + $ch_dm + $dm_ch;
+            $ch_dm      = $_POST['Climate']['ch_dm'];
+            $dm_ch      = 1 / $ch_dm;
+            $dm_dm      = 1;
 
+            $sum_column_ch      = $ch_ch + $ch_temp + $ch_dm;
+            $sum_column_temp    = $temp_ch + $temp_temp + $temp_dm;
+            $sum_column_dm      = $dm_ch + $dm_temp + $dm_dm;
+
+            /* ---- */
+
+            $divided_sum_sum        = $sum_column_ch / $sum_column_ch;
+
+            $divided_ch_ch_sum      = $ch_ch / $sum_column_ch;
+            $divided_ch_temp_sum    = $ch_temp / $sum_column_ch;
+            $divided_ch_dm_sum      = $ch_dm / $sum_column_ch;
+
+            $divided_temp_ch_sum    = $temp_ch / $sum_column_temp;
+            $divided_temp_temp_sum  = $temp_temp / $sum_column_temp;
+            $divided_temp_dm_sum    = $temp_dm / $sum_column_temp;
+
+            $divided_dm_ch_sum      = $dm_ch / $sum_column_dm;
+            $divided_dm_temp_sum    = $dm_temp / $sum_column_dm;
+            $divided_dm_dm_sum      = $dm_dm / $sum_column_dm;
+
+            /* ---- */
+
+            $sum_ch             = $divided_ch_ch_sum + $divided_temp_ch_sum + $divided_dm_ch_sum;
+            $sum_temp           = $divided_ch_temp_sum + $divided_temp_temp_sum + $divided_dm_temp_sum;
+            $sum_dm             = $divided_ch_dm_sum + $divided_temp_dm_sum + $divided_dm_dm_sum;
+            $sum_divided        = $divided_sum_sum + $divided_sum_sum + $divided_sum_sum;
+
+            /* ---- */
+
+            $bobot_ch           = $sum_ch / $sum_divided;
+            $bobot_temp         = $sum_temp / $sum_divided;
+            $bobot_dm           = $sum_dm / $sum_divided;
+
+            echo "bobot CH : " . $bobot_ch . ". bobot temp : " . $bobot_temp . ". bobot dm : " . $bobot_dm;
+            echo "<br> Total bobot =" . ($bobot_ch + $bobot_temp + $bobot_dm);
+            /* ---- */
             die();
 
             //$model->load(Yii::$app->request->post()) && $model->save()
