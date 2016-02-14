f2e7bc0a54d8202a815eba2d5a8cd8bcca3a0dcc
ketuker <you@local.com>
2016-02-14 17:02:59 +0700

    baru sedikit menambahkan relasi user

--- config/web.php
+++ config/web.php
@@ -125,8 +125,12 @@ if (YII_ENV_DEV) {
     ];
 
     $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
+    //$config['modules']['gii'] = [
+    //    'class' => 'yii\gii\Module',
+    //];
+	$config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.1.*'] // adjust this to your needs
     ];
 }
 
