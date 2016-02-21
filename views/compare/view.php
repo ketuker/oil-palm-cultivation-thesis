4c1f6641583f1aebcc7a0a3886d27a4e1bfcb9a8
Habib Royni Mujtahid <habib@bukapeta.com>
2016-02-21 16:40:23 +0700

    Add legenda @view/aoi

--- views/compare/view.php
+++ views/compare/view.php
@@ -10,6 +10,33 @@ $this->title = $model->title;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app','Area of Interest'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<style>
+    .info {
+        padding: 6px 8px;
+        font: 14px/16px Arial, Helvetica, sans-serif;
+        background: white;
+        background: rgba(255,255,255,0.8);
+        box-shadow: 0 0 15px rgba(0,0,0,0.2);
+        border-radius: 5px;
+    }
+    .info h4 {
+        margin: 0 0 5px;
+        color: #777;
+    }
+
+    .legend {
+        text-align: left;
+        line-height: 18px;
+        color: #555;
+    }
+    .legend i {
+        width: 18px;
+        height: 18px;
+        float: left;
+        margin-right: 8px;
+        opacity: 0.7;
+    }
+</style>
 <div class="compare-view">
 
     <div class="list-group-item"><h4 class="list-group-item-heading"><b><center><?= $model->title;?></center></b></h4></div>
@@ -279,8 +306,6 @@ $script .= '
         style: style,
     }).addTo(map);
 
-
-
     map.fitBounds(RendergeojsonDraw.getBounds());
 
     layercontrol.addOverlay(RendergeojsonFeature, "Hasil");
@@ -297,6 +322,20 @@ $script .= '
     layercontrol.addOverlay(RendergeojsonFeatureTown, "Distance From Town");
 
     layercontrol.addOverlay(RendergeojsonDraw, "Area Of Interest");
+
+    /** Legenda **/
+    var legend = L.control({position: "bottomright"});
+
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create("div", "info legend"),
+            grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+            labels = [];
+        div.innerHTML = \'<i style="background:#E31A1C"></i>Very Suitable<br><i style="background:#54ff00"></i>Suitable<br><i style="background:#fffc00"></i>Mostly Suitable<br><i style="background:#ff7a00"></i>Not Suitable<br>\';
+        return div;
+    };
+
+    legend.addTo(map);
+
 ';
 
 $this->registerJs($script, \yii\web\View::POS_END); ?>
